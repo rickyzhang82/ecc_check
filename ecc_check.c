ad6c8caac99e1b269a69c3eeb6d2be64b6938144
Ricky Zhang <ricky.zhang@sas.com>
2014-07-23 08:06:39 -0400

    Add ECC check comment

--- ecc_check.c
+++ ecc_check.c
@@ -9,7 +9,12 @@
  
 #define MEM_START       0xFED10000
 #define FILESIZE        32*1024
- 
+/**
+ * 0: ECC disabled
+ * 1: ECC is active in I/O; ECC logic is not active In this case.
+ * 2: ECC is disabled in I/O, but ECC logic is enabled.
+ * 3: ECC active in both I/O and ECC logic
+ */ 
 int main(int argc, char *argv[])
 {
     int i;
